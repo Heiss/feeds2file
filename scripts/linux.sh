4e74760588dbac2f1386ffe428dad1d3140b3077
iPhysicX <physicx@netzmuffel.de>
2019-03-07 14:11:29 +0100

    ups, scripts not renamed, done

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -4,7 +4,7 @@ time=3600
 while :
 do 
 	echo "Start crawling."
-	../feed2file
+	../feeds2file
 	echo "Stop crawling."
 	echo "infinite loop - next run in ${time} seconds [ hit CTRL+C to stop]"
 	sleep ${time}
--- scripts/win.bat
+++ scripts/win.bat
@@ -4,7 +4,7 @@ SET time=3600
 
 :while
 	echo "Start crawling."
-	start ../feed2file.exe
+	start ../feeds2file.exe
 	echo "Stop crawling."
 	echo "infinite loop - next run in %time% seconds [ hit CTRL+C to stop]"
 	timeout /T %time%  > nul
